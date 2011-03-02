7677c3c827b37f47d7ee6ef4aeff998633d69ddd
cekk <andrea.cecchi85@gmail.com>
2011-03-02 11:01:32 +0000

    some fixes

--- rer/subsites/content/subsite.py
+++ rer/subsites/content/subsite.py
@@ -1,16 +1,39 @@
 # -*- coding: utf-8 -*-
 """Definition of the subsite content type
 """
-from Products.ATContentTypes.content import folder
+from Products.ATContentTypes.content import schemata, folder
 from Products.ATContentTypes.content.base import registerATCT
+from Products.ATReferenceBrowserWidget.ATReferenceBrowserWidget import \
+    ReferenceBrowserWidget
+from Products.Archetypes import atapi
+from rer.subsites import subsitesMessageFactory as _
 from rer.subsites.config import PROJECTNAME
 from rer.subsites.interfaces import IRERSubsite
 from zope.interface import implements
+
+RERSubsiteSchema = folder.ATFolderSchema.copy() + atapi.Schema((
+    
+    atapi.ReferenceField("newsArchiveFolder",
+             storage=atapi.AnnotationStorage(),
+             required=False,
+             multiValued=False,
+             relationship='newsArchiveFolder',
+             widget=ReferenceBrowserWidget(
+                            label=_('rer_subsites_newsarchivefolder', default=u'News archive'),
+                            description=_('rer_subsites_newsarchivefolder_help', default=u'An archive for the news. If this field is set, the news in this subsite will show his value in "News archive" link'),
+                            )
+             ),
+))
+
+RERSubsiteSchema['title'].storage = atapi.AnnotationStorage()
+RERSubsiteSchema['description'].storage = atapi.AnnotationStorage()
+schemata.finalizeATCTSchema(RERSubsiteSchema, moveDiscussion=False)
 
 class RERSubsite(folder.ATFolder):
     """Folder for RERSubsite"""
     implements(IRERSubsite)
     meta_type = "RERSubsite"
+    schema = RERSubsiteSchema
     
     def allowedContentTypes(self):
         '''
---
+++ rer/subsites/exportimport.py
@@ -0,0 +1,20 @@
+from Products.CMFCore.utils import getToolByName
+
+def import_various(context):
+    if context.readDataFile('ersubsites-various.txt') is None:
+        return
+    
+    portal = context.getSite()
+    addPropertySheet(portal)
+    
+def addPropertySheet(portal):
+    portal_properties = getToolByName(portal, 'portal_properties')
+    er_news_archive_properties = getattr(portal_properties, 'er_news_archive_properties',None)
+    if not er_news_archive_properties:
+        portal_properties.addPropertySheet(id='er_news_archive_properties',title='News archives properties')
+        portal.plone_log("Added news archives properties property-sheet")
+        er_news_archive_properties = getattr(portal_properties, 'er_news_archive_properties',None)
+    if not er_news_archive_properties.hasProperty('type_filter'):
+        er_news_archive_properties.manage_addProperty('type_filter',"RERSubsite", 'string')
+    else:
+        er_news_archive_properties.manage_changeProperties(type_filter='RERSubsite')
