c704d7ffab837bf6f8a02feccf21674dfb07741e
cekk <andrea.cecchi85@gmail.com>
2011-03-28 09:42:14 +0000

    aggiunto sfondo e immagine

--- rer/subsites/browser/viewlets.py
+++ rer/subsites/browser/viewlets.py
@@ -1,22 +1,44 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.common import ViewletBase
 
-class SubsiteTitleViewlet(ViewletBase):
-    index = ViewPageTemplateFile('viewlets/rer_subsite_title.pt')
-    
+class SubsiteViewletBase(ViewletBase):
     def __init__(self, context, request, view, manager):
-        super(SubsiteTitleViewlet, self).__init__(context, request, view, manager)
-        self.subsite_title=self.getSubsiteTitle()
-        
+        super(SubsiteViewletBase, self).__init__(context, request, view, manager)
+        self.subsite=self.getSubsiteObj()
+    
     def render(self):
-        if self.subsite_title:
+        if self.subsite:
             return self.index()
         else:
             return ""
     
-    def getSubsiteTitle(self):
+    def getSubsiteObj(self):
         for elem in self.context.aq_inner.aq_chain:
             if getattr(elem,'portal_type','') == 'RERSubsite':
-                return elem.Title()
+                return elem
+        return None
     
+class SubsiteTitleViewlet(SubsiteViewletBase):
+    index = ViewPageTemplateFile('viewlets/rer_subsite_title.pt')
     
+class SubsiteColorViewlet(SubsiteViewletBase):
+    """
+    A Viewlet that allows to add some dynamic css in the  header
+    """
+    def render(self):
+        if not self.subsite:
+            return ""
+        color=self.subsite.getSubsiteColor()
+        image=self.subsite.getImage()
+        return_string = ''
+        if color or image:
+            styles=[]
+            css="#subsiteTitle {"
+            if color:
+                styles.append("background-color:%s" %color)
+            if image:
+                styles.append("background-image:url(%s)" %image.absolute_url())
+            css +=';'.join(styles)
+            css +='}'
+            return_string = "<style type='text/css'>%s</style>" %css
+        return return_string
--- rer/subsites/content/subsite.py
+++ rer/subsites/content/subsite.py
@@ -10,6 +10,10 @@ from rer.subsites import subsitesMessageFactory as _
 from rer.subsites.config import PROJECTNAME
 from rer.subsites.interfaces import IRERSubsite
 from zope.interface import implements
+from Products.validation.config import validation
+from Products.validation.interfaces import ivalidator
+from Products.validation.validators.SupplValidators import MaxSizeValidator
+from Products.validation import V_REQUIRED
 
 RERSubsiteSchema = folder.ATFolderSchema.copy() + atapi.Schema((
     
@@ -23,6 +27,29 @@ RERSubsiteSchema = folder.ATFolderSchema.copy() + atapi.Schema((
                             description=_('rer_subsites_newsarchivefolder_help', default=u'An archive for the news. If this field is set, the news in this subsite will show his value in "News archive" link'),
                             )
              ),
+    atapi.ImageField('image',
+            widget=atapi.ImageWidget(
+                label=_(u'rer_subsites_image', default=u'Image'),
+                description=_(u'rer_subsites_image_help', default=u"Insert an image for the viewlet with the subsite name."),
+            ),
+            storage=atapi.AttributeStorage(),
+            max_size=(768,768),
+            sizes= {'large'   : (768, 768),
+                    'preview' : (400, 400),
+                    'mini'    : (200, 200),
+                    'thumb'   : (128, 128),
+                    'tile'    :  (64, 64),
+                    'icon'    :  (32, 32),
+                    'listing' :  (16, 16),
+                      },
+            validators = (('isNonEmptyFile', V_REQUIRED)),
+            ),
+    atapi.StringField('subsiteColor',
+                required=True,
+                widget=atapi.StringWidget(label=_(u'rer_subsites_color', default=u'Color of the subsite'),
+                                    description=_(u'rer_subsites_color_help', default=u"Insert an hexadecimal value for the subsite color. Use the # character before the  value (for example: #FFFFFF)"),
+                                    )
+               ),
 ))
 
 RERSubsiteSchema['title'].storage = atapi.AnnotationStorage()
