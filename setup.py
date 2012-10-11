8f81e3208f21b013f76d6e860ffd177e165999ea
keul <luca@keul.it>
2012-10-11 13:51:26 +0000

    Preparing release 1.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ This module contains the tool of rer.subsites
 import os
 from setuptools import setup, find_packages
 
-version = '1.2.0.dev0'
+version = '1.2.0'
 
 tests_require = ['zope.testing']
 
