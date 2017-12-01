5ce1874d74a27bb65590841f34db99760f45debf
daniele-andreotti <daniele.andreotti@redturtle.it>
2017-12-01 17:55:00 +0100

    Added classes for subsites into the body

---
+++ rer/subsites/browser/layout.py
@@ -0,0 +1,35 @@
+from plone import api
+from plone.app.layout.globals.layout import LayoutPolicy as BaseView
+
+
+class LayoutPolicy(BaseView):
+    """A view that gives access to various layout related functions.
+    """
+
+    def bodyClass(self, template, view):
+        """
+        Returns the CSS class to be used on the body tag.
+
+        Included body classes
+        - template name: template-{}
+        - portal type: portaltype-{}
+        - navigation root: site-{}
+        - section: section-{}
+            - only the first section
+        - section structure
+            - a class for every container in the tree
+        - hide icons: icons-on
+        - markspeciallinks: pat-markspeciallinks
+        - userrole-{} for each role the user has in this context
+        - min-view-role: role required to view context
+        - default-view: if view is the default view
+        """
+        body_classes = super(LayoutPolicy, self).bodyClass(template, view)
+        view = api.content.get_view(name='subsite_utils_view', context=self.context, request=self.request)
+        subsite = view.get_subsite_folder()
+        if self.context == subsite:
+            body_classes+= ' subsite-root'
+        if getattr(self.context, 'subsite_class', ''):
+            subsite_class_name = ' subsite-%s' % getattr(self.context, 'subsite_class')
+            body_classes+= subsite_class_name
+        return body_classes
