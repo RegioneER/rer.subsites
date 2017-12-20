61c7d5b8dfbfe8628a8693e0f39ce45dd1a05d13
Andrea Cecchi <andrea.cecchi85@gmail.com>
2017-12-20 09:27:53 +0100

    add upgrades

---
+++ rer/subsites/upgrades/upgrades.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from rer.erpolicy import logger
+
+
+default_profile = 'profile-rer.erpolicy:default'
+
+
+def to_2100(context):
+    """
+    """
+    logger.info('Upgrading rer.erpolicy to version 2100')
+    context.runImportStepFromProfile(default_profile, 'rolemap')
+    context.runImportStepFromProfile(default_profile, 'actions')
