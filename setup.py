2f4716ffbf4ed417649134172ae2b062ea82ca75
Andrea Cecchi <andrea.cecchi85@gmail.com>
2017-12-20 09:54:45 +0100

    Preparing release 1.3.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ This module contains the tool of rer.subsites
 import os
 from setuptools import setup, find_packages
 
-version = '2.0.0.dev0'
+version = '1.3.2'
 
 setup(name='rer.subsites',
       version=version,
