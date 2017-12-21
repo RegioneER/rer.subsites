2c20068fd87a855da954365127551283698fb98c
Andrea Cecchi <andrea.cecchi85@gmail.com>
2017-12-21 09:47:10 +0100

    fix typo

--- rer/subsites/upgrades/upgrades.py
+++ rer/subsites/upgrades/upgrades.py
@@ -5,12 +5,12 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-default_profile = 'profile-rer.erpolicy:default'
+default_profile = 'profile-rer.subsites:default'
 
 
 def to_2100(context):
     """
     """
-    logger.info('Upgrading rer.erpolicy to version 2100')
+    logger.info('Upgrading rer.subsites to version 2100')
     context.runImportStepFromProfile(default_profile, 'rolemap')
     context.runImportStepFromProfile(default_profile, 'actions')
