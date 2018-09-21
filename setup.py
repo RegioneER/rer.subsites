eb17604409567f96bec3c17a59a0dafd61226fa9
Andrea Cecchi <andrea.cecchi85@gmail.com>
2018-09-21 15:17:21 +0200

    Back to development: 1.4.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ This module contains the tool of rer.subsites
 import os
 from setuptools import setup, find_packages
 
-version = '1.4.1'
+version = '1.4.2.dev0'
 
 setup(name='rer.subsites',
       version=version,
